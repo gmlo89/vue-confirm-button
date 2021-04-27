883e12c35d8660d7799e41f31ea3a8646da85e5b
Giovanni Lopez <gmlo89@hotmail.com>
2021-04-27 00:27:18 -0600

    wip

---
+++ src/VueConfirmButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button>My button</button>
+</template>
+
+<script>
+export default {
+    name: 'VueConfirmButton',
+    mounted(){
+        console.log('Montado');
+    }
+}
+</script>
+
+<style>
+
+</style>
